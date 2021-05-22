fd8644627c484a4eb7a47f66830db4044a7d4b58
Damie <jogungbure@gmail.com>
2021-05-22 23:25:20 +0100

    Added the signup route and function

--- database/migrations/1621721363088_users.ts
+++ database/migrations/1621721363088_users.ts
@@ -6,11 +6,10 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id").primary();
-      table.string("name").notNullable;
+      table.string("name").notNullable();
       table.string("email", 255).notNullable();
       table.string("password", 180).notNullable();
       table.string("remember_me_token").nullable();
-
       table.dateTime("created_at");
       table.dateTime("updated_at");
     });
