b79bb228802d12dfefea6cc9cf16c2171ea1144c
Damie <jogungbure@gmail.com>
2021-05-23 23:14:12 +0100

    Added the route guard

--- app/Controllers/Http/BasicsController.ts
+++ app/Controllers/Http/BasicsController.ts
@@ -1,7 +1,11 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class BasicsController {
-  public index({ view }: HttpContextContract) {
-    return view.render("general/home");
+  public index({ view, auth, response }: HttpContextContract) {
+    if (auth.user) {
+      return response.redirect("/tasks");
+    } else {
+      return view.render("general/home");
+    }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -22,14 +22,16 @@ import Route from "@ioc:Adonis/Core/Route";
 
 Route.get("/", "BasicsController.index");
 
-Route.get("/signup", "AuthController.signup").as("signup");
+Route.get("/signup", "AuthController.signup").as("signup").middleware("guest");
 Route.post("/signup", "AuthController.createUser");
-Route.get("/login", "AuthController.login").as("login");
+Route.get("/login", "AuthController.login").as("login").middleware("guest");
 Route.post("/login", "AuthController.authenticate");
 Route.post("/logout", "AuthController.logout");
 
-Route.get("/tasks", "TasksController.index");
-Route.post("/task", "TasksController.addTask");
-Route.patch("/tasks/check/:id", "TasksController.markCompleted");
-Route.patch("/tasks/uncheck/:id", "TasksController.markUncompleted");
-Route.delete("/tasks/:id", "TasksController.deleteTask");
+Route.group(() => {
+  Route.get("/tasks", "TasksController.index");
+  Route.post("/task", "TasksController.addTask");
+  Route.patch("/tasks/check/:id", "TasksController.markCompleted");
+  Route.patch("/tasks/uncheck/:id", "TasksController.markUncompleted");
+  Route.delete("/tasks/:id", "TasksController.deleteTask");
+}).middleware("auth");
